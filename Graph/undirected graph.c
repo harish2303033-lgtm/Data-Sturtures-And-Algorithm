a02b8dc2beb7237a33ac017e97d4daffb481fd84
harish-k <harish.2303033@srec.ac.in>
2026-01-02 11:15:00 +0530

    undirected graph using adjacency matrix.c
    This implementation allows for the creation of an undirected graph using an adjacency matrix. It includes functions to add edges and print the adjacency matrix.

---
+++ Graph/undirected graph.c
@@ -0,0 +1,43 @@
+//Undirected Graph Implementation Using Array (Adjacency Matrix)
+
+#include <stdio.h>
+
+#define MAX 10
+
+int adjMatrix[MAX][MAX];
+
+void addEdge(int u,int v)
+{
+    adjMatrix[u][v]=1;
+    adjMatrix[v][u]=1;
+}
+
+void printGraph(int vertices)
+{
+    printf("Adjacency Matrix : \n");
+    for(int i=0;i<vertices;i++)
+    {
+        for(int j=0;j<vertices;j++)
+        {
+            printf("%d ",adjMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int edge,vertices,u,v;
+    printf("Enter number of vertices : ");
+    scanf("%d",&vertices);
+    printf("Enter number of edges : ");
+    scanf("%d",&edge);
+    for(int i=0;i<edge;i++)
+    {
+        printf("Enter edge (u v) : ");
+        scanf("%d %d",&u,&v);
+        addEdge(u,v);
+    }
+    printGraph(vertices);
+}
